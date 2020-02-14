578a5b6067b02a3a674b2faf13b56fcc7d413547
김윤후 <gim-yunhu@gim-yunhuui-MacBookPro.local>
2020-02-14 23:32:46 +0900

    다형성

--- test.js
+++ test.js
@@ -1,8 +1,29 @@
-function add_maker(a){
-    return function(b){
-        return(a+b)
+const users = [
+    { id: 1, name: 'ID', age: 36 },
+    { id: 2, name: 'BJ', age: 32 },
+    { id: 3, name: 'JM', age: 32 },
+    { id: 4, name: 'PJ', age: 27 },
+    { id: 5, name: 'HA', age: 25 },
+    { id: 6, name: 'JE', age: 26 },
+    { id: 7, name: 'JI', age: 31 },
+    { id: 8, name: 'MP', age: 23 }
+  ];
+
+function _each(list, iter) {
+    for(let i = 0; i < list.length; i++){
+        iter(list[i])
     }
+    return list
+}
+function _filter(list, predi) {
+    const new_list = []
+    _each(list, (val) => {if(predi(val)) new_list.push(val)})
+    return new_list
+}
+function _map(list, mapper) {
+    const new_list = []
+    _each(list, (val) => {new_list.push(mapper(val))})
+    return new_list
 }
 
-const add10 = add_maker(10)
-console.log(add10(20))
+console.log(_filter(users, (val) => val.age > 30))
