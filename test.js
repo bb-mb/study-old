02e48c8dc2556d0cb0607047bb7fb99cbeab41db
soso01 <6125024@gmail.com>
2020-02-16 13:55:59 +0900

    섹션3

--- test.js
+++ test.js
@@ -1,20 +1,17 @@
+var users = [
+  { id: 10, name: 'ID', age: 36 },
+  { id: 20, name: 'BJ', age: 32 },
+  { id: 30, name: 'JM', age: 32 },
+  { id: 40, name: 'PJ', age: 27 },
+  { id: 50, name: 'HA', age: 25 },
+  { id: 60, name: 'JE', age: 26 },
+  { id: 70, name: 'JI', age: 31 },
+  { id: 80, name: 'MP', age: 23 },
+  { id: 90, name: 'FP', age: 13 }
+];
 
-function _is_object(obj){
-  return typeof(obj) === 'object' && !!obj
-}
-function _keys(obj){
-  return _is_object(obj) ? Object.keys(obj) : []
+function _values(data){
+  return data.map(function(val) {return val})
 }
 
-function _each(list, iter){
-  const keys = _keys(list)
-  for (let i = 0, len = keys.length; i < len; i++){
-    iter(list[keys[i]])
-  }
-}
-
-_each({
-  13 : 'ID',
-  19 : 'HD',
-  29 : 'YD',
-  }, function(name){ console.log(name)})
+console.log(_values(users))
