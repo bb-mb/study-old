c094874b0914b4c1c96bb74d8317b290ef04942c
soso01 <6125024@gmail.com>
2021-02-22 11:56:31 +0900

    react-spring 써보기

---
+++ test-react-spring/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="desktop-background">
+      <div className="App">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
